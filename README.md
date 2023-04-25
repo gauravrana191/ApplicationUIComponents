82d1cf41e9600b325ccb84c2448e7a4889aeae82
Gaurav Kumar <gauravrana191@gmail.com>
2023-04-25 20:13:25 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ApplicationUIComponents
+
+A description of this package.
---
+++ Tests/ApplicationUIComponentsTests/ApplicationUIComponentsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ApplicationUIComponents
+
+final class ApplicationUIComponentsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ApplicationUIComponents().text, "Hello, World!")
+    }
+}
