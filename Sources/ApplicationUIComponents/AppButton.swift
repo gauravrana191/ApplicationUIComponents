2cda153e7414e2eeef1c6b1b96eb8f696b922c4c
Gaurav Kumar <gauravrana191@gmail.com>
2023-04-25 23:19:59 +0530

    commit message from terminal

--- Sources/ApplicationUIComponents/AppButton.swift
+++ Sources/ApplicationUIComponents/AppButton.swift
@@ -1,5 +1,5 @@
 //
-//  SwiftUIView.swift
+//  AppButton.swift
 //  
 //
 //  Created by Gaurav Kumar on 25/04/23.
@@ -7,14 +7,32 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+@available(macOS 10.15, *)
+struct NewButton<Content: View> : View {
+    let content : ()-> Content
+    let action: () -> Void
+
+    init(action: @escaping () -> Void, @ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+        self.action = action
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: self.action) {
+            content()
+        }
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+
+@available(macOS 10.15, *)
+struct NewButton_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        NewButton {
+            print("Hello")
+        } content: {
+            Text("")
+        }
+
     }
 }
